d3c6f8a25bdfc697eac56d6d526cc792e2c352b6
Asami Jonsson <momoji303@gmail.com>
2022-02-21 15:41:48 +0100

    Homepage2

--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -1,21 +1,23 @@
-// import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 // import Card from "./Card";
 
 const Homepage = () => {
-  //   const cards = useSelector((state) => state.cards);
+  const cards = useSelector((state) => state.cardInfo);
+  console.log(cards.cardInfo);
   return (
     <div>
       <h2>Cards</h2>
+      <h2>{cards.cardInfo[0].cardNumber}</h2>
       {/* <ul>
         {cards.map((card) => (
           <Card {...card} id={card.id} isFront={card.isFront} />
         ))}
       </ul> */}
-      {/* <Link to={`/add/`}> */}
-      <button>ADD A NEW CARD</button>
-      {/* </Link> */}
+      <Link to={`/add/`}>
+        <button>ADD A NEW CARD</button>
+      </Link>
     </div>
   );
 };
