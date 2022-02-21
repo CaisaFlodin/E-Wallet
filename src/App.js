c8d7709e23ee0ddac9be2e6043ef9df4f9a730f1
Asami Jonsson <momoji303@gmail.com>
2022-02-21 15:47:23 +0100

    Home3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./page/Homepage";
+import addCard from "./page/addCard";
+
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/add/" render={(props) => <addCard {...props} />} />
         <Route path="/" render={(props) => <Homepage {...props} />} />
-        <Route />
       </Switch>
     </div>
   );
