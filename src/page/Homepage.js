c9e5049079922dc00b14535e5eb358d87d6d4551
Caisa Flodin <caisaflodin@hotmail.com>
2022-02-24 15:22:34 +0100

    removed unnecessary content

--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -6,22 +6,6 @@ const Homepage = () => {
       <h2>Wallet:</h2>
       <Cards />
     </>
-    // <div>
-    //   <h2>This card is Active</h2>
-    //   {cardList.map((card, i) => {
-    //     return <Card key={i} {...card} />;
-    //   })}
-    //   {cardList.length <= 3 ? (
-    //     <Link to={"/add/"}>
-    //       <button>Add a new card</button>
-    //     </Link>
-    //   ) : (
-    //     <p>
-    //       Your wallet have the maximum amount of 4 cards. Remove cards to add a
-    //       new one.
-    //     </p>
-    //   )}
-    // </div>
   );
 };
 export default Homepage;
