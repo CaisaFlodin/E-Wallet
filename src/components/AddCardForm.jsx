a050a896eb732fd7b581d3abf7048a9698db69b3
Oscar Hederlig <oscar.hederlig@gmail.com>
2022-02-25 09:51:16 +0100

    .

--- src/components/AddCardForm.jsx
+++ src/components/AddCardForm.jsx
@@ -18,18 +18,14 @@ const AddCardForm = () => {
   const formatAndSetcardNumber = e => {
     const inputVal = e.target.value.replace(/ /g, "");
     let inputNumbersOnly = inputVal.replace(/\D/g, "");
-
     if (inputNumbersOnly.length > 16) {
       inputNumbersOnly = inputNumbersOnly.substr(0, 16);
     }
-
     const splits = inputNumbersOnly.match(/.{1,4}/g);
-
     let spacedNumber = "";
     if (splits) {
       spacedNumber = splits.join(" ");
     }
-
     setCardNumber(spacedNumber);
   };
 
