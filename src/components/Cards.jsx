b35e9f714afb7a80c1fc8ae5af80d89761e45b89
Asami Jonsson <momoji303@gmail.com>
2022-02-28 09:47:12 +0100

    active card2

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,10 @@
 import Card from "./Card";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import cardFour from "../images/card4.png";
+import cardThree from "../images/card3.png";
+import cardTwo from "../images/card2.png";
+import cardOne from "../images/card1.png";
 
 const Cards = () => {
   const { cardList, activeObj } = useSelector((state) => state.cards);
@@ -11,75 +15,46 @@ const Cards = () => {
       <div>
         <h2>This card is Active</h2>
         {activeObj === cardList[0].id ? (
-          <p>
-            {cardList[0].cardNumber}
-            <br />
-            {cardList[0].cardHolderName}
-            <br />
-            {cardList[0].validMonth}
-
-            {cardList[0].validYear}
-            <br />
-            {cardList[0].cvc}
-            <br />
-            {cardList[0].vendor}
-          </p>
+          <div className="card4">
+            <img className="cardImg" src={cardFour} alt="" width="450px" />
+            <p className="cardNumber4"> {cardList[0].cardNumber}</p>
+            <p className="cardHolderName4">{cardList[0].cardHolderName}</p>
+            <p className="validMonth4"> {cardList[0].validMonth}</p>
+            <p className="validYear4"> {cardList[0].validYear}</p>
+            <p className="cvc4">{cardList[0].cvc}</p>
+            <p>{cardList[0].vendor}</p>
+          </div>
         ) : activeObj === cardList[1].id ? (
-          <p>
-            {cardList[1].cardNumber}
-            <br />
-            {cardList[1].cardHolderName}
-            <br />
-            {cardList[1].validMonth}
+          <div>
+            <img className="cardImg" src={cardThree} alt="" width="450px" />
 
-            {cardList[1].validYear}
-            <br />
-            {cardList[1].cvc}
-            <br />
-            {cardList[1].vendor}
-          </p>
+            <p className="cardNumber3"> {cardList[1].cardNumber}</p>
+            <p className="cardHolderName3">{cardList[1].cardHolderName}</p>
+            <p className="validMonth3"> {cardList[1].validMonth}</p>
+            <p className="validYear3"> {cardList[1].validYear}</p>
+            <p className="cvc3">{cardList[1].cvc}</p>
+            <p> {cardList[1].vendor}</p>
+          </div>
         ) : activeObj === cardList[2].id ? (
-          <p>
-            {cardList[2].cardNumber}
-            <br />
-            {cardList[2].cardHolderName}
-            <br />
-            {cardList[2].validMonth}
-
-            {cardList[2].validYear}
-            <br />
-            {cardList[2].cvc}
-            <br />
-            {cardList[2].vendor}
-          </p>
+          <div>
+            <img className="cardImg" src={cardTwo} alt="" width="450px" />
+            <p className="cardNumber2"> {cardList[2].cardNumber}</p>
+            <p className="cardHolderName2">{cardList[2].cardHolderName}</p>
+            <p className="validMonth2"> {cardList[2].validMonth}</p>
+            <p className="validYear2"> {cardList[2].validYear}</p>
+            <p className="cvc2">{cardList[2].cvc}</p>
+            <p>{cardList[2].vendor}</p>
+          </div>
         ) : activeObj === cardList[3].id ? (
-          <p>
-            {cardList[3].cardNumber}
-            <br />
-            {cardList[3].cardHolderName}
-            <br />
-            {cardList[3].validMonth}
-
-            {cardList[3].validYear}
-            <br />
-            {cardList[3].cvc}
-            <br />
-            {cardList[3].vendor}
-          </p>
-        ) : activeObj === cardList[4].id ? (
-          <p>
-            {cardList[4].cardNumber}
-            <br />
-            {cardList[4].cardHolderName}
-            <br />
-            {cardList[4].validMonth}
-
-            {cardList[4].validYear}
-            <br />
-            {cardList[4].cvc}
-            <br />
-            {cardList[4].vendor}
-          </p>
+          <div>
+            <img className="cardImg" src={cardOne} alt="" width="450px" />
+            <p className="cardNumber1"> {cardList[3].cardNumber}</p>
+            <p className="cardHolderName1">{cardList[3].cardHolderName}</p>
+            <p className="validMonth1"> {cardList[3].validMonth}</p>
+            <p className="validYear1"> {cardList[3].validYear}</p>
+            <p className="cvc1">{cardList[3].cvc}</p>
+            <p>{cardList[3].vendor}</p>
+          </div>
         ) : null}
 
         {cardList.map((card, i) => {
